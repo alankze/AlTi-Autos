f73e435655a111e71a5a0ced26f1db75ad43da99
alankze <alan.zin.eddin@gmail.com>
2023-08-25 11:21:43 +0200

    components

---
+++ client/src/components/Main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Cars from './Cars';
+import NewCar from './NewCar';
+import CarDetails from './CarDetails';
+import UpdateCar from './UpdateCar';
+
+const Main = () => {
+  return <main>
+    <Routes>
+        <Route path="/" element={<Cars />} />
+        <Route path="cars/new" element={<NewCar />} />
+        <Route path="cars/:id" element={<CarDetails />} />
+        <Route path="cars/:id/update" element={<UpdateCar />} />
+    </Routes>
+  </main>;
+};
+
+export default Main
