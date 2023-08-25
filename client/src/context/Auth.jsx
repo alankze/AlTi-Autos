d1dcfaab44343cfed80b2e5c27cea03a9decac15
quasargetter <timjanzarik@web.de>
2023-08-25 12:59:26 +0200

    "frontend auth"

---
+++ client/src/context/Auth.jsx
@@ -0,0 +1,70 @@
+import {createContext, useState, useEffect} from 'react';
+import axios from '../axiosinstance';
+import { useNavigate } from 'react-router-dom';
+
+export const AuthContext = createContext();
+
+
+
+ const AuthProvider = ({children}) => {
+    const[user,setUser] = useState(null)
+    const [loading, setLoading] = useState(true)
+    const [errors,setErrors] = useState(null)
+
+    const setState = (user,loading,errors)=> {
+        setUser(user)
+        setLoading(loading)
+        setErrors(errors)
+    };
+
+    useEffect(()=> {
+        axios.get("auth/currentUser").then(res => setState(res.data.user,false, null))
+        .catch(error => {
+           setState(null,false,null)
+        })
+    }, []);
+    const login = async user => {
+        setLoading(true);
+        try {
+            const res = await axios.post("auth/login", user);
+            setState(res.data.user, false, null);
+            navigate('/');
+        } catch (error) {
+            console.log(error.response);
+            setState(null, false, error.response.data);
+            
+        }
+        
+    }
+    const register = async user => {
+        setLoading(true);
+        try {
+            const res = await axios.post("auth/register", user);
+            setState(res.data.user, false, null);
+            navigate('/');
+        } catch (error) {
+            console.log(error.response);
+            setState(null, false, error.response.data);
+            
+        }
+        
+    }
+    const logout = async () => {
+        setLoading(true);
+        try {
+            const res = await axios.post("auth/logout", {});
+            setState(null, false, null);
+            navigate('/');
+            window.location.reload();
+        } catch (error) {
+            console.log(error.response);
+            setState(null, false, error.response.data);
+            
+        }
+        
+    }
+    
+
+  return <AuthContext.Provider value={user,loading,errors}>{children}</AuthContext.Provider>
+  
+}
