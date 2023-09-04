44d279e6147e44c707e1fd80cbd679712bfff075
quasargetter <timjanzarik@web.de>
2023-09-05 01:56:57 +0200

    "changed Login/Register to default because it work"

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,4 +1,12 @@
+import {
+  MDBContainer,
+  MDBInput,
+  MDBCheckbox,
+  MDBBtn,
+  MDBIcon,
+} from "mdb-react-ui-kit";
 import { useState, useContext } from "react";
+import React from "react";
 import { AuthContext } from "../context/Auth";
 import { Navigate } from "react-router-dom";
 import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
@@ -25,109 +33,10 @@ function Login() {
   if (!context.loading && context.user) {
     return <Navigate to="/cars" />;
   }
-
   if (!context.loading && !context.user) {
     return (
-      <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-md">
-          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-            Login to your account
-          </h2>
-        </div>
-        <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-          <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-            <form className="space-y-6" onSubmit={handleSubmit}>
-              <div>
-                <label
-                  htmlFor="email"
-                  className="block text-sm font-medium text-gray-700"
-                >
-                  Email address
-                </label>
-                <div className="mt-1">
-                  <input
-                    type="email"
-                    name="email"
-                    value={user.email}
-                    onChange={handleChange}
-                    required
-                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-                  />
-                </div>
-              </div>
-              <div>
-                <label
-                  htmlFor="password"
-                  className="block text-sm font-medium text-gray-700"
-                >
-                  Password
-                </label>
-                <div className="mt-1 relative">
-                  <input
-                    type="password"
-                    name="password"
-                    value={user.password}
-                    onChange={handleChange}
-                    required
-                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-                  />
-                  {visible ? (
-                    <AiOutlineEye
-                      className="absolute right-2 top-2 cursor-pointer"
-                      size={25}
-                      onClick={() => setVisible(false)}
-                    />
-                  ) : (
-                    <AiOutlineEyeInvisible
-                      className="absolute right-2 top-2 cursor-pointer"
-                      size={25}
-                      onClick={() => setVisible(true)}
-                    />
-                  )}
-                </div>
-              </div>
-              <div className={`${styles.normalFlex} justify-between`}>
-                <div className={`${styles.normalFlex}`}>
-                  <input
-                    type="checkbox"
-                    name="remember-me"
-                    id="remember-me"
-                    className="h-4 w-4 text-blue-600 focus:ring-blue-500 border-gray-300 rounded"
-                  />
-                  <label
-                    htmlFor="remember-me"
-                    className="ml-2 block text-sm text-gray-900"
-                  >
-                    Remember me
-                  </label>
-                </div>
-                <div className="text-sm">
-                  <a
-                    href=".forgot-password"
-                    className="font-medium text-blue-600 hover:text-blue-500"
-                  >
-                    Forgot your password?
-                  </a>
-                </div>
-              </div>
-              <div>
-                <button
-                  type="submit"
-                  className="group relative w-full h-[40px] flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700"
-                >
-                  Submit
-                </button>
-              </div>
-              <div className={`${styles.normalFlex} w-full`}>
-                <h4>Not have any account?</h4>
-                <Link to="/register" className="text-blue-600 pl-2">
-                  Sign Up
-                </Link>
-              </div>
-            </form>
-          </div>
-        </div>
-        {/*{context.errors?.message}
+      <>
+        {context.errors?.message}
         <form onSubmit={handleSubmit}>
           <label htmlFor="">Email:</label>
           <input
@@ -146,8 +55,8 @@ function Login() {
             required
           />
           <button>Login</button>
-        </form>*/}
-      </div>
+        </form>
+      </>
     );
   }
 }
